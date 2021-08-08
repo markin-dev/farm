bada0219a830b4a87e7bb26efeb9da7ec5e1d09f
Сергей Маркин <markin.dev@gmail.com>
2021-08-08 17:07:50 +0400

    added FloatText component

---
+++ src/components/FloatText.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="float-text">
+    <transition
+      appear
+      name="float-text"
+    >
+      <span
+        class="float-text__value"
+        :style="{transition: `all ${duration}s`}"
+      >
+        {{ text }}
+      </span>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+
+    duration: {
+      type: Number,
+      default: 0.6,
+    },
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.$emit('expired');
+    }, this.duration * 1000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.float-text {
+  display: inline;
+
+  &__value {
+    position: absolute;
+    display: inline-block;
+    color: $green-500;
+    opacity: 0;
+  }
+
+  &-enter-active {
+    opacity: 1;
+  }
+
+  &-enter-to {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+}
+</style>
--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -1,36 +1,62 @@
 <template>
   <div class="game-field">
-    <div class="game-field__left-menu" />
+    <div class="game-field__left-menu">
+      dummy
+    </div>
     <div
       class="game-field__main-field"
     >
       <Scythe
         class="scythe"
-        @click="harvest"
+        @mousedown-on-scythe="harvest"
       />
-      <div>Crops: {{ crops }}</div>
+      <div>
+        Crops: {{ crops }}&nbsp;
+        <FloatText
+          v-for="(item) in incomeTextItems"
+          :key="item.id"
+          :text="item.value"
+          @expired="temp"
+        />
+      </div>
     </div>
-    <div class="game-field__right-menu" />
+    <div class="game-field__right-menu">
+      dummy
+    </div>
   </div>
 </template>
 
 <script>
 import Scythe from '@/components/Scythe.vue';
+import FloatText from '@/components/FloatText.vue';
 
 export default {
   components: {
     Scythe,
+    FloatText,
   },
 
   data() {
     return {
       crops: 0,
+      incomeTextItems: [],
+      counter: 0,
     };
   },
 
   methods: {
     harvest() {
-      this.crops += 1;
+      this.incomeTextItems.push({
+        id: Math.random(),
+        value: `+${this.counter}`,
+      });
+
+      this.crops += this.counter;
+      this.counter += 1;
+    },
+
+    temp() {
+      this.incomeTextItems.shift();
     },
   },
 };
