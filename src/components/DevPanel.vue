03bad1dec924164d12590f2dd288ec054a437e10
Сергей Маркин <markin.dev@gmail.com>
2021-08-12 15:23:59 +0400

    added the ability to resize the dev panel

--- src/components/DevPanel.vue
+++ src/components/DevPanel.vue
@@ -1,19 +1,20 @@
 <template>
   <div
     v-if="!off"
-    ref="devPanel"
     class="dev-panel"
     :style="styleObject"
   >
-    <div style="overflow: hidden">
-      <div
-        class="dev-panel__header"
-        @mousedown="startDrag"
-      />
-      <div class="dev-panel__content">
-        <slot />
-      </div>
+    <div
+      class="dev-panel__header"
+      @mousedown="startDrag"
+    />
+    <div class="dev-panel__content">
+      <slot />
     </div>
+    <div
+      class="dev-panel__scale"
+      @mousedown="startScale"
+    />
   </div>
 </template>
 
@@ -32,6 +33,9 @@ export default {
       offsetLeft: 0,
       shiftX: 0,
       shiftY: 0,
+      panelWidth: 300,
+      panelHeight: 400,
+      gapFromBounds: 8,
     };
   },
 
@@ -40,14 +44,28 @@ export default {
       return {
         top: `${this.offsetTop}px`,
         left: `${this.offsetLeft}px`,
+        width: `${this.panelWidth}px`,
+        height: `${this.panelHeight}px`,
       };
     },
+
+    isOutOfBoundsAlongX() {
+      return this.offsetLeft + this.panelWidth + this.gapFromBounds > window.innerWidth;
+    },
+
+    isOutOfBoundsAlongY() {
+      return this.offsetTop + this.panelHeight + this.gapFromBounds > window.innerHeight;
+    },
+  },
+
+  mounted() {
+    this.checkoutOfBoundsForDrag();
   },
 
   methods: {
     startDrag(event) {
       document.onmousemove = this.dragElement;
-      document.onmouseup = this.stopdrag;
+      document.onmouseup = this.removeDocumentEvents;
 
       this.shiftX = event.clientX - this.offsetLeft;
       this.shiftY = event.clientY - this.offsetTop;
@@ -59,30 +77,53 @@ export default {
       this.offsetLeft = event.clientX - this.shiftX;
       this.offsetTop = event.clientY - this.shiftY;
 
-      this.checkoutOfBounds();
+      this.checkoutOfBoundsForDrag();
     },
 
-    checkoutOfBounds() {
-      const devPanelWidth = this.$refs.devPanel.clientWidth;
-      const devPanelHeight = this.$refs.devPanel.clientHeight;
+    startScale(event) {
+      document.onmousemove = this.scaleElement;
+      document.onmouseup = this.removeDocumentEvents;
 
-      if (this.offsetLeft < 0) {
-        this.offsetLeft = 0;
-      }
-      if (this.offsetTop < 0) {
-        this.offsetTop = 0;
-      }
-      if (this.offsetLeft + devPanelWidth > window.innerWidth) {
-        this.offsetLeft = window.innerWidth - devPanelWidth;
-      }
-      if (this.offsetTop + devPanelHeight > window.innerHeight) {
-        this.offsetTop = window.innerHeight - devPanelHeight;
-      }
+      this.shiftX = event.clientX - this.panelWidth;
+      this.shiftY = event.clientY - this.panelHeight;
     },
 
-    stopdrag() {
+    scaleElement(event) {
+      event.preventDefault();
+
+      this.panelWidth = event.clientX - this.shiftX;
+      this.panelHeight = event.clientY - this.shiftY;
+
+      this.checkoutOfBoundsForScale();
+    },
+
+    removeDocumentEvents() {
       document.onmouseup = null;
       document.onmousemove = null;
+    },
+
+    checkoutOfBoundsForDrag() {
+      if (this.offsetLeft < this.gapFromBounds) {
+        this.offsetLeft = this.gapFromBounds;
+      }
+      if (this.offsetTop < this.gapFromBounds) {
+        this.offsetTop = this.gapFromBounds;
+      }
+      if (this.isOutOfBoundsAlongX) {
+        this.offsetLeft = window.innerWidth - this.panelWidth - this.gapFromBounds;
+      }
+      if (this.isOutOfBoundsAlongY) {
+        this.offsetTop = window.innerHeight - this.panelHeight - this.gapFromBounds;
+      }
+    },
+
+    checkoutOfBoundsForScale() {
+      if (this.isOutOfBoundsAlongX) {
+        this.panelWidth = window.innerWidth - this.offsetLeft - this.gapFromBounds;
+      }
+      if (this.isOutOfBoundsAlongY) {
+        this.panelHeight = window.innerHeight - this.offsetTop - this.gapFromBounds;
+      }
     },
   },
 };
@@ -91,22 +132,35 @@ export default {
 <style lang="scss" scoped>
 .dev-panel {
   position: fixed;
-  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  min-width: 64px;
+  min-height: 64px;
   z-index: 9999;
 
   &__header {
-    height: 32px;
+    flex: 0 0 32px;
     width: 100%;
     background-color: $gray-300;
     cursor: move;
   }
 
   &__content {
-    max-width: 400px;
-    max-height: 700px;
     padding: 16px;
     background-color: $gray-200;
+    height: 100%;
     overflow: scroll;
+  }
+
+  &__scale {
+    position: absolute;
+    width: 12px;
+    height: 12px;
+    bottom: 0;
+    right: 0;
+    border-right: 4px solid $gray-500;
+    border-bottom: 4px solid $gray-500;
+    cursor: nwse-resize;
   }
 }
 </style>
