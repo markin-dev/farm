b3ad6c885ae21b01d2a5e9b7279d5dc28d16a2a1
Сергей Маркин <markin.dev@gmail.com>
2022-02-09 14:54:30 +0400

    added "about" modal

---
+++ src/components/AboutModal.vue
@@ -0,0 +1,129 @@
+<template>
+  <FPopup
+    class="about-modal"
+    :is-open="isOpen"
+    title="Farm"
+    @close="$emit('close')"
+  >
+    <p>Simple farming clicker based on Vue</p>
+    <a
+      href="https://github.com/markin-dev/farm"
+      target="_blank"
+    >
+      GitHub
+    </a>
+    <div class="about-modal__attribution">
+      <p>Attribution:</p>
+      <a
+        v-for="linkData in $options.ATTRIBUTION_LINKS_DATA"
+        :key="linkData.text"
+        target="_blank"
+        :title="linkData.title"
+        :href="linkData.href"
+      >
+        {{ linkData.text }}
+      </a>
+    </div>
+  </FPopup>
+</template>
+
+<script>
+import FPopup from '@/components/FPopup.vue';
+
+const ATTRIBUTION_LINKS_DATA = [
+  {
+    text: 'Harvest icons created by Eucalyp - Flaticon',
+    title: 'harvest icons',
+    href: 'https://www.flaticon.com/free-icons/harvest',
+  },
+  {
+    text: 'Chicken icons created by Freepik - Flaticon',
+    title: 'chicken icons',
+    href: 'https://www.flaticon.com/free-icons/chicken',
+  },
+  {
+    text: 'Rabbit icons created by Freepik - Flaticon',
+    title: 'rabbit icons',
+    href: 'https://www.flaticon.com/free-icons/rabbit',
+  },
+  {
+    text: 'Pig icons created by dDara - Flaticon',
+    title: 'pig icons',
+    href: 'https://www.flaticon.com/free-icons/pig',
+  },
+  {
+    text: 'Goat icons created by Freepik - Flaticon',
+    title: 'goat icons',
+    href: 'https://www.flaticon.com/free-icons/goat',
+  },
+  {
+    text: 'Cow icons created by surang - Flaticon',
+    title: 'cow icons',
+    href: 'https://www.flaticon.com/free-icons/cow',
+  },
+  {
+    text: 'Wheat icons created by DinosoftLabs - Flaticon',
+    title: 'wheat icons',
+    href: 'https://www.flaticon.com/free-icons/wheat',
+  },
+  {
+    text: 'Potato icons created by Freepik - Flaticon',
+    title: 'potato icons',
+    href: 'https://www.flaticon.com/free-icons/potato',
+  },
+  {
+    text: 'Vegetable icons created by Freepik - Flaticon',
+    title: 'vegetable icons',
+    href: 'https://www.flaticon.com/free-icons/vegetable',
+  },
+  {
+    text: 'Corn icons created by mangsaabguru - Flaticon',
+    title: 'corn icons',
+    href: 'https://www.flaticon.com/free-icons/corn',
+  },
+  {
+    text: 'Fruit icons created by Freepik - Flaticon',
+    title: 'fruit icons',
+    href: 'https://www.flaticon.com/free-icons/fruit',
+  },
+];
+
+export default {
+  ATTRIBUTION_LINKS_DATA,
+
+  components: {
+    FPopup,
+  },
+
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.about-modal {
+  p {
+    margin-bottom: 16px;
+  }
+
+  a {
+    display: block;
+    margin-bottom: 16px;
+  }
+
+  &__attribution {
+    p {
+      margin-bottom: 8px;
+    }
+
+    a {
+      margin-bottom: 8px;
+      font-size: 12px;
+    }
+  }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,55 @@
 <template>
   <div class="header">
-    <h1>Farm</h1>
-    <div class="actions">
-      <FLink
-        class="actions__item"
-        :text="'Save'"
-        @click="manualSave"
-      />
+    <div class="header__content">
+      <h1>Farm</h1>
+      <div class="actions">
+        <FLink
+          class="actions__item"
+          text="Save"
+          @click="manualSave"
+        />
+        <FLink
+          class="actions__item"
+          text="About"
+          @click="openAboutPopup"
+        />
+      </div>
     </div>
+    <AboutModal
+      :is-open="isAboutPopupOpen"
+      @close="closeAboutPopup"
+    />
   </div>
 </template>
 
 <script>
 import FLink from '@/components/FLink.vue';
-
+import AboutModal from '@/components/AboutModal.vue';
 import { save } from '@/saveLoad';
 
 export default {
   components: {
     FLink,
+    AboutModal,
+  },
+
+  data() {
+    return {
+      isAboutPopupOpen: false,
+    };
   },
 
   methods: {
     manualSave() {
       save(this.$store);
+    },
+
+    openAboutPopup() {
+      this.isAboutPopupOpen = true;
+    },
+
+    closeAboutPopup() {
+      this.isAboutPopupOpen = false;
     },
   },
 };
@@ -31,14 +57,16 @@ export default {
 
 <style lang="scss" scoped>
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 8px 24px;
-  height: 64px;
-  border-bottom: 1px solid $gray-300;
-  background-color: $gray-50;
-  box-sizing: border-box;
+  &__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 24px;
+    height: 64px;
+    border-bottom: 1px solid $gray-300;
+    background-color: $gray-50;
+    box-sizing: border-box;
+  }
 
   .actions {
     &__item:not(:last-child) {
