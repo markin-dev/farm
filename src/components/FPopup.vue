bd2b09aa621ce96408d05d9e77c3f36ee997aca0
Сергей Маркин <markin.dev@gmail.com>
2022-02-09 14:54:15 +0400

    added popup component

---
+++ src/components/FPopup.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    v-if="isOpen"
+    class="f-popup"
+  >
+    <div
+      class="f-popup__overlay"
+      @click="close"
+    >
+      <div
+        class="f-popup__body"
+        @click.stop
+      >
+        <div class="f-popup__header">
+          <h2 class="f-popup__title">
+            {{ title }}
+          </h2>
+          <CloseButton @click="close" />
+        </div>
+        <div class="f-popup__content">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CloseButton from '@/components/CloseButton.vue';
+
+export default {
+  components: {
+    CloseButton,
+  },
+
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.f-popup {
+  &__overlay {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+    cursor: pointer;
+  }
+
+  &__body {
+    background-color: white;
+    cursor: auto;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    width: 100%;
+    box-sizing: border-box;
+    border-bottom: 1px solid $gray-300;
+    background-color: $gray-50;
+  }
+
+  &__title {
+    margin: 0;
+    line-height: 1;
+  }
+
+  &__content {
+    padding: 16px;
+  }
+}
+</style>
