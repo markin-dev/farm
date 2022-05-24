800a4ef8f20eda9518790addb2781af7d750ee9c
Sergey Markin <markin.dev@gmail.com>
2022-05-25 01:53:34 +0400

    removed vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import store from '@/store';
 import formatMoney from '@/utils/formatMoney';
 import App from './App.vue';
 import '@/scss/normalize.scss';
@@ -7,7 +6,6 @@ import startGameLoop from '@/newStore/gameLoop';
 
 const app = createApp(App);
 
-app.use(store);
 app.config.globalProperties.$formatMoney = formatMoney;
 app.mount('#app');
 
