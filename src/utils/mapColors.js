9b450de4d8902dc381bdedf22f384263804a23f6
Sergey Markin <markin.dev@gmail.com>
2022-05-27 16:59:28 +0400

    added map colors

---
+++ src/utils/mapColors.js
@@ -0,0 +1,95 @@
+// Gray
+export const $gray50 = '#F9FAFB';
+export const $gray100 = '#F3F4F6';
+export const $gray200 = '#E5E7EB';
+export const $gray300 = '#D1D5DB';
+export const $gray400 = '#9CA3AF';
+export const $gray500 = '#6B7280';
+export const $gray600 = '#4B5563';
+export const $gray700 = '#374151';
+export const $gray800 = '#1F2937';
+export const $gray900 = '#111827';
+
+// Red
+export const $red50 = '#FEF2F2';
+export const $red100 = '#FEE2E2';
+export const $red200 = '#FECACA';
+export const $red300 = '#FCA5A5';
+export const $red400 = '#F87171';
+export const $red500 = '#EF4444';
+export const $red600 = '#DC2626';
+export const $red700 = '#B91C1C';
+export const $red800 = '#991B1B';
+export const $red900 = '#7F1D1D';
+
+// Yellow
+export const $yellow50 = '#FFFBEB';
+export const $yellow100 = '#FEF3C7';
+export const $yellow200 = '#FDE68A';
+export const $yellow300 = '#FCD34D';
+export const $yellow400 = '#FBBF24';
+export const $yellow500 = '#F59E0B';
+export const $yellow600 = '#D97706';
+export const $yellow700 = '#B45309';
+export const $yellow800 = '#92400E';
+export const $yellow900 = '#78350F';
+
+// Green
+export const $green50 = '#ECFDF5';
+export const $green100 = '#D1FAE5';
+export const $green200 = '#A7F3D0';
+export const $green300 = '#6EE7B7';
+export const $green400 = '#34D399';
+export const $green500 = '#10B981';
+export const $green600 = '#059669';
+export const $green700 = '#047857';
+export const $green800 = '#065F46';
+export const $green900 = '#064E3B';
+
+// Blue
+export const $blue50 = '#EFF6FF';
+export const $blue100 = '#DBEAFE';
+export const $blue200 = '#BFDBFE';
+export const $blue300 = '#93C5FD';
+export const $blue400 = '#60A5FA';
+export const $blue500 = '#3B82F6';
+export const $blue600 = '#2563EB';
+export const $blue700 = '#1D4ED8';
+export const $blue800 = '#1E40AF';
+export const $blue900 = '#1E3A8A';
+
+// Indigo
+export const $indigo50 = '#EEF2FF';
+export const $indigo100 = '#E0E7FF';
+export const $indigo200 = '#C7D2FE';
+export const $indigo300 = '#A5B4FC';
+export const $indigo400 = '#818CF8';
+export const $indigo500 = '#6366F1';
+export const $indigo600 = '#4F46E5';
+export const $indigo700 = '#4338CA';
+export const $indigo800 = '#3730A3';
+export const $indigo900 = '#312E81';
+
+// Purple
+export const $purple50 = '#F5F3FF';
+export const $purple100 = '#EDE9FE';
+export const $purple200 = '#DDD6FE';
+export const $purple300 = '#C4B5FD';
+export const $purple400 = '#A78BFA';
+export const $purple500 = '#8B5CF6';
+export const $purple600 = '#7C3AED';
+export const $purple700 = '#6D28D9';
+export const $purple800 = '#5B21B6';
+export const $purple900 = '#4C1D95';
+
+// Pink
+export const $pink50 = '#FDF2F8';
+export const $pink100 = '#FCE7F3';
+export const $pink200 = '#FBCFE8';
+export const $pink300 = '#F9A8D4';
+export const $pink400 = '#F472B6';
+export const $pink500 = '#EC4899';
+export const $pink600 = '#DB2777';
+export const $pink700 = '#BE185D';
+export const $pink800 = '#9D174D';
+export const $pink900 = '#831843';
